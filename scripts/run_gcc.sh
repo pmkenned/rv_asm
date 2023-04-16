9e1dd9458abc8ced641edb1d5df2dee9941405e8
Paul Kennedy <paul.kennedy124@gmail.com>
2023-04-16 06:45:23 -0500

    Updated scripts

--- scripts/run_gcc.sh
+++ scripts/run_gcc.sh
@@ -7,16 +7,32 @@ filename=$(basename -- "$1")
 extension="${filename##*.}"
 filename="${filename%.*}"
 
-MARCH=rv32ifdc
-MABI=ilp32 
-riscv64-unknown-elf-gcc -march=$MARCH -mabi=$MABI -c $1 -o out/$filename.o
-riscv64-unknown-elf-gcc -march=$MARCH -mabi=$MABI -Wl,-T linker_script.lds -nostdlib out/$filename.o -o out/$filename
-#riscv64-unknown-elf-objdump -d $filename.o
-#riscv64-unknown-elf-objdump -d $filename
+XLEN="${2:-32}"
 
-#MARCH=rv64ifdc
-#MABI=lp64 
-#riscv64-unknown-elf-gcc -march=$MARCH -mabi=$MABI -c $1 -o out/$filename.o
-#riscv64-unknown-elf-gcc -Wl,-T linker_script.lds -nostdlib out/$filename.o -o out/$filename
-##riscv64-unknown-elf-objdump -d $filename.o
-##riscv64-unknown-elf-objdump -d $filename
+MARCH=rv${XLEN}ifdc
+if [[ $XLEN -eq "32" ]]
+then
+    MABI=ilp${XLEN}
+elif [[ $XLEN -eq "64" ]]
+then
+    MABI=lp64
+else
+    echo "error: invalid XLEN"
+    exit 1
+fi
+
+# compile and link
+riscv64-unknown-elf-gcc -march=$MARCH -mabi=$MABI -c $1 -o out/$filename.o
+#riscv64-unknown-elf-gcc -march=$MARCH -mabi=$MABI -Wl,--verbose -nostdlib out/$filename.o -o out/$filename
+riscv64-unknown-elf-gcc -march=$MARCH -mabi=$MABI -Wl,-T scripts/linker_script.lds -nostdlib out/$filename.o -o out/$filename
+
+# readelf
+riscv64-unknown-elf-readelf -rseW out/$filename.o > out/$filename.o.readelf
+riscv64-unknown-elf-readelf -rseW out/$filename > out/$filename.readelf
+
+~/github/c/subprojects/elf/build/elfread out/$filename.o > out/$filename.o.readelf.mine
+~/github/c/subprojects/elf/build/elfread out/$filename > out/$filename.readelf.mine
+
+# disassemble
+riscv64-unknown-elf-objdump -d -t -r out/$filename.o > out/$filename.o.dasm
+riscv64-unknown-elf-objdump -d -t -r out/$filename > out/$filename.dasm
